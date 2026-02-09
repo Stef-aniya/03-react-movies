26e457d3d0fa8bc40a9e1e30219c9f08a08828f4
Stef-aniya <stefaniyamomot@gmail.com>
2026-02-09 16:53:05 +0200

    Gallery

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import css from "./App.module.css";
-import type { Movie } from "./types/movie";
-import { fetchMovies } from "./services/movieService";
+import type { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
 import { Toaster, toast } from "react-hot-toast";
-import SearchBar from "./components/SearchBar/SearchBar";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieModal from "./components/MovieModal/MovieModal";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 import { createPortal } from "react-dom";
 
 export default function App() {
